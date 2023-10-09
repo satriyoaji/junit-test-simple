846f910bff5fcaed7282250d2f1769aab0d1afbe
satriyoaji <satriyoaji27@gmail.com>
2023-10-09 23:55:24 +0700

    add Unit test ParameterizedTest function

--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +13,18 @@ class CalculatorTest {
     @DisplayName("Add two numbers")
     void add() {
         assertEquals(4, Calculator.add(2, 2));
+    }
+
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+    void addFromCSV(int first, int second, int expectedResult) {
+        assertEquals(expectedResult, Calculator.add(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
     }
 
     @org.junit.jupiter.api.Test
